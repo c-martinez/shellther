b72d9ec36500a52cf68b4d886379c13f2f6ab474
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-11-27 13:21:17 +0100

    Documentation

--- config.js
+++ config.js
@@ -2,13 +2,25 @@ var dummyConfig = {
   engine: 'dummy',
 };
 
+// Configuration for working with Etherpad
 var etherpadConfig = {
   engine: 'etherpad',
+
+  // API key of your etherpad (from APIKEY.txt)
   apikey: 'e792c32e44952f8d24c2cabe35bf36a12003d04726d3579c36f5a1d00569c81c',
+
+  // Address where your etherpad is hosted
   host: 'localhost',
+
+  // Port on which etherpad runs
   port: 9001,
+
+  // Name of the target pad you want to write to
   padID: 'scshell-test',
 };
 
+// TODO: Add configurations for other engines (if available)
+
+// TODO: export whichever configuration you would like to use
 // module.exports = dummyConfig;
 module.exports = etherpadConfig;
--- index.js
+++ index.js
@@ -2,28 +2,12 @@
 
 var spawn  = require('child_process').spawn;
 var fs     = require('fs');
+
+// Load config and desired engine
 var config = require('./config');
 var engine = require('./lib/' + config.engine);
 
-// Taken from: https://groups.google.com/forum/#!topic/nodejs/b9JdFrJQqn0
-function shell(cmd, opts, callback) {
-  process.stdin.pause();
-  process.stdin.setRawMode(false);
-
-  var child = spawn(cmd, opts, {
-    stdio: 'inherit',
-  });
-  child.on('exit', function() {
-    process.stdin.setRawMode(true);
-    process.stdin.resume();
-    engine.exitAction(function() {
-      process.exit();
-    });
-  });
-
-  return child;
-}
-
+// Trigger to call engine.timedAction every N seconds
 var timer;
 function triggerTimer() {
   if (!timer) {
@@ -34,17 +18,42 @@ function triggerTimer() {
   }
 }
 
-var userArgs = process.argv.slice(2);
-var savefile = userArgs[0];
+// Taken from: https://groups.google.com/forum/#!topic/nodejs/b9JdFrJQqn0
+// Start a terminal which logs output to given file and setup for engine
+// functions to be called
+function shellLogger(savefile) {
+  process.stdin.pause();
+  process.stdin.setRawMode(false);
 
-if (savefile === undefined) {
-  savefile = 'somefile.txt';
+  // Launch logging terminal
+  var child = spawn('script', ['-f', savefile], {
+    stdio: 'inherit',
+  });
+
+  // Setup engine.exitAction trigger
+  child.on('exit', function() {
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    engine.exitAction(function() {
+      process.exit();
+    });
+  });
+
+  // Setup engine.timedAction trigger
+  fs.watch(savefile, triggerTimer);
 }
 
+// Pass in savefile from command line (or use default: shellLog.txt)
+var userArgs = process.argv.slice(2);
+var savefile = userArgs[0];
+if (savefile === undefined) {
+  savefile = 'shellLog.txt';
+}
+
+// Configure engine
 config.savefile = savefile;
 engine.setup(config);
 
+// Launch shell
 var timeout = 5;  // in seconds
-
-shell('script', ['-f', savefile]);
-fs.watch(savefile, triggerTimer);
+shellLogger(savefile);
