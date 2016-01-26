e033f149e2ca43e187dd6a1bc11ba205df51036c
Carlos Martinez <c.martinez@esciencecenter.nl>
2016-01-26 11:52:09 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+    name="shelter",
+    version="0.0.1",
+    description="Shell logger and sync to Etherpad",
+    long_description="Shell logger and sync to Etherpad",
+    author="Software Carpentry",
+    # url="https://github.com/c-martinez/sc-shell/",
+    packages=["shelter", "shelter.scripts"],
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+    **extra)
--- shelter/main.py
+++ shelter/main.py
@@ -1,23 +1,5 @@
-"""Shelter. Log your terminal and sync it with Etherpad (or other repository).
-
-Usage:
-  main.py <padID> [--config <configfile>] --dedicated
-  main.py <padID> [--config <configfile>] --section [--marker <marker>]
-
-Options:
-  -h --help     Show this screen.
-  --dedicated   Use dedicated Etherpad to log console
-  --section     Use a section in the Etherpad to log console
-  --config <configfile>  Load configuration from config file (default: .config).
-  --marker <marker>  Marker used as separator. If None, random marker is generated (default: random).
-"""
-
 import os
 import sys
-
-from docopt import docopt
-from ConfigParser import SafeConfigParser
-from tempfile import NamedTemporaryFile
 
 from engines import EtherpadFullEngine, EtherpadSectionEngine
 
@@ -40,25 +22,3 @@ def doMain(apikey, padID, marker, baseurl, logfile, doSection):
     else:
         engine = EtherpadFullEngine(apikey, padID, targetFile=logfile, base_url=baseurl)
     recordConsole(engine, logfile)
-
-if __name__=='__main__':
-    args = docopt(__doc__, version='Shelter v0.1')
-
-    logfile = NamedTemporaryFile(delete=True).name
-    print 'Using temp file: ',logfile
-    padID = args['<padID>']
-    doSection = args['--section']
-    marker = args['--marker']
-
-    defaultConfig = {
-            'apikey'  : 'no-api-key',
-            'baseurl': 'http://localhost:9001/api'
-        }
-    config = SafeConfigParser(defaultConfig)
-    config.add_section('shelter')
-    if args['--config']:
-        config.read(args['--config'])
-    apikey = config.get('shelter', 'apikey')
-    baseurl = config.get('shelter', 'baseurl')
-
-    doMain(apikey, padID, marker, baseurl, logfile, doSection)
